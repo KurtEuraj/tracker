e27b148f51bbfb14e5ac46aa9b6a48f71e1c2218
Kurt Euraj <kurteurajdev@gmail.com>
2024-04-11 22:11:33 -0400

    Add toggleable autocomplete search results

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,11 @@
 import "./SearchBar.scss"
 import searchIcon from "../../assets/images/search.svg"
 
-function SearchBar({ handleSearch, updateSearchTerm }) {
+function SearchBar({ handleSearch, updateSearchTerm, query }) {
   return (
     <form className="search-bar" onSubmit={handleSearch}>
       <div className="search-bar__wrapper">
-        <input className="search-bar__input" name="song" type="text" placeholder="Enter a song 🎧" onChange={updateSearchTerm}/>
+        <input className="search-bar__input" name="song" type="text" placeholder="Enter a song 🎧" onChange={updateSearchTerm} value={query} required/>
         <button type="submit" className="search-bar__submit-button">
           <img src={searchIcon} alt="search icons" className="search-bar__submit-icon" />
         </button>
